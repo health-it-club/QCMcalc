c591da5332b0a3d4f4caa4148dab2b90013de5b8
mArOOu <marouanemebarekfalouti@gmail.com>
2025-05-06 20:08:43 +0100

    Export module settings fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
+import { output } from "motion/react-client";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+};
+
+module.exports = {
+  output: "export",
 };
 
 export default nextConfig;
